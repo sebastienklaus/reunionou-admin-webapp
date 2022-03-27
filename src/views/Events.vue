e236c7756d6ff3da80cef31bc22b09b801b11ea7
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-27 21:24:38 +0200

    add events page

---
+++ src/views/Events.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <h1 class="title has-text-centered">Listes des événements</h1>
+        <div class="columns is-multiline is-mobile">
+            <div
+                class="column is-half-tablet is-half-desktop is-full-mobile"
+                v-for="event in sortedArray"
+                :key="event.id"
+            >
+                <Event :event="event" @deleteEvent="deleteEvent"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Event from "../components/EventCard.vue";
+
+export default {
+    components: {
+        Event,
+    },
+    data() {
+        return {
+            events: [],
+        };
+    },
+    mounted() {
+        this.getEvents();
+    },
+    methods: {
+        getEvents(){
+            this.$api
+            .get("events")
+            .then((response) => {
+                this.events = response.data.events;
+            })
+            .catch((err) => console.log(err));
+        },
+        deleteEvent(id){
+            this.$api
+                .delete("events/" + id)
+                .then((response) => {
+                    console.log('delete succes!');
+                    this.getEvents();
+                })
+                .catch((err) => console.log(err));
+        }
+    },
+    computed: {
+        sortedArray: function() {
+            function compare(a, b) {
+            if (a.date < b.date)
+                return -1;
+            if (a.date > b.date)
+                return 1;
+            return 0;
+            }
+
+            return this.events.sort(compare);
+        }
+    },
+};
+</script>
+
+<style></style>
